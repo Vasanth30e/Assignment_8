071ba9a8f978f697fa2114e3bfa4971b7c1ae9e9
unknown <vasanthgamer2000@gmail.com>
2023-07-17 15:30:19 +0530

    Salary Calculation

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8
+{
+    public class MonthSalary
+    {
+        public double SalaryCalc(double wHour, double nWdays, double projectHandles = 1, double extras = 0)
+        {
+            double monthlySalary = wHour * nWdays * 100 + projectHandles * 3000 + extras * 2000;
+            return monthlySalary;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MonthSalary monthlySalary = new MonthSalary();
+
+            string typeOfEmployee;
+            Console.WriteLine("Enter employee type: \n 1 for HR \n 2 for Admin \n 3 for Software Developer");
+            typeOfEmployee = (Console.ReadLine());
+
+            int workingHour, noOfWorkingDAys, projectHandles, extras;
+           
+            switch (typeOfEmployee)
+            {
+                case "HR":
+                    Console.WriteLine("Enter no. of Working Hour");
+                    workingHour = int.Parse(Console.ReadLine());    
+
+                    Console.WriteLine("Enter no. of Working Days");
+                    noOfWorkingDAys = int.Parse(Console.ReadLine());
+
+                    projectHandles = 1;
+                    extras = 0;
+
+                    Console.WriteLine("Monthly Salary for HR is " + monthlySalary.SalaryCalc(workingHour, noOfWorkingDAys, projectHandles, extras));
+                    break;
+
+                case "Admin":
+                    Console.WriteLine("Enter no. of Working Hour for Admin ");
+                    workingHour = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter no. of Working Days for Admin");
+                    noOfWorkingDAys = int.Parse(Console.ReadLine());
+
+                    
+                    extras = 0;
+
+
+
+                    Console.WriteLine("Monthly Salary for Admin is " + monthlySalary.SalaryCalc( workingHour, noOfWorkingDAys, extras));
+                    break;
+
+
+                case "SD":
+
+                    Console.WriteLine("Enter no. of Working Hour");
+                    workingHour = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter no. of Working Days");
+                    noOfWorkingDAys = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter no. of Project Handles");
+                    projectHandles = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter no. of Extras");
+                    extras = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Monthly Salary for Software Developer is " + monthlySalary.SalaryCalc(workingHour, noOfWorkingDAys, projectHandles, extras));
+                    break;
+                
+                default:
+                    Console.WriteLine("Employee Type Not Exist");
+                    break;
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
